f618ab5ff206ec365085e8be207ea1fbb0575bc3
pouyabakhshi <pouyabakhshiinfo@gmail.com>
2024-06-25 00:36:00 +0330

    final release

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
-     <Toaster   position="top-center" reverseOrder={false}/>
+      <Toaster position="top-center" reverseOrder={false} />
       <App />
     </Provider>
   </React.StrictMode>
