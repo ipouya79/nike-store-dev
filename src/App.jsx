3c78f79ced26a7e1c642b1f6f5d956bae4db7097
pouyabakhshi <pouyabakhshiinfo@gmail.com>
2024-06-24 14:19:56 +0330

    Finish UI Design

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import {
+  Cart,
+  FlexContent,
+  Footer,
+  Hero,
+  Navbar,
+  Sales,
+  Stories,
+} from "./components";
+import {
+  footerAPI,
+  heroapi,
+  highlight,
+  popularsales,
+  sneaker,
+  story,
+  topratesales,
+} from "./data/data";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={topratesales} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </>
+  );
+};
+
+export default App;
